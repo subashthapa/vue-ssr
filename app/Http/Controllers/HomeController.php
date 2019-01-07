aa83f0b59800827283169ececd3ce67d0a861ffb
Subash Thapa <info.subashthapa@gmail.com>
2019-01-08 09:56:40 +1100

    SSR Setup

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,28 @@
+<?php 
+
+namespace App\Http\Controllers;
+
+class HomeController extends Controller
+{
+	private function render($path) {
+    $renderer_source = File::get(base_path('node_modules/vue-server-renderer/basic.js'));
+    $app_source = File::get(public_path('js/entry-server.js'));
+    $v8 = new \V8Js();
+    ob_start();
+    $js = 
+<<<EOT
+var process = { env: { VUE_ENV: "server", NODE_ENV: "production" } }; 
+this.global = { process: process }; 
+var url = "$path";
+EOT;
+    $v8->executeString($js);
+    $v8->executeString($renderer_source);
+    $v8->executeString($app_source);
+    return ob_get_clean();
+  }
+
+  public function get(Request $request) {
+    $ssr = $this->render($request->path());
+    return view('welcome', ['ssr' => $ssr]);
+  }
+}
